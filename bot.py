2ec1adc8c57df70bbbbf0d01b1c5054aeaa3b647
lalas51 <67890416+lalas51@users.noreply.github.com>
2020-08-03 20:24:29 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,14 @@
+import telebot;
+bot = telebot.TeleBot('%1381323596:AAHvVjPBkfUcURunqak8YkBxtxSCJmvolnw%');
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+@bot.message_handler(content_types=['text', 'document', 'audio'])
+if message.text == "Zdarova":
+    bot.send_message(message.from_user.id, "Zdarova, чем я могу тебе помочь?")
+elif message.text == "/help":
+    bot.send_message(message.from_user.id, "Напиши Zdarova Хряк")
+else:
+    bot.send_message(message.from_user.id, "Я тебя не понимаю, воин. Напиши /help.")
+	bot.polling(none_stop=True, interval=0)
